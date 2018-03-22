c4fde3f2d7f231f89e802a399332022e116eaa33
canhlinh <canhlinh.lienson@gmail.com>
2018-03-22 12:08:04 +0700

    Limit maximize of executions time

--- src/models/job_stats.go
+++ src/models/job_stats.go
@@ -6,8 +6,9 @@ import (
 
 // JobStats for storing job metrics
 type JobStats struct {
-	SuccessCount   int        `json:"success_count" bson:"success_count"`
-	ErrorCount     int        `json:"error_count" bson:"error_count"`
+	ScheduleCount  uint       `json:"schedule_count"`
+	SuccessCount   uint       `json:"success_count" bson:"success_count"`
+	ErrorCount     uint       `json:"error_count" bson:"error_count"`
 	LastRanAt      *time.Time `json:"last_ran_at" bson:"last_ran_at"`
 	LastSuccededAt *time.Time `json:"last_succeded_at" bson:"last_succeded_at"`
 	LastError      string     `json:"last_error" bson:"last_error"`
